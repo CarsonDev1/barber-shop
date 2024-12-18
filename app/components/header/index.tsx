1134dc444b22b560ce4808af3875a2d1e33daff5
CarsonDev1 <buitritinht@gmail.com>
2024-12-18 14:28:51 +0700

    upload code

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -45,6 +45,7 @@ export default function Header() {
 	const [notificationsOpen, setNotificationsOpen] = useState(false);
 	const router = useRouter();
 	const [tokenExchange, setTokenExchange] = useState<string | null>(null);
+	const [dataDecode, setDataDecode] = useState<any>();
 
 	const exchangeToken = async (token: string) => {
 		try {
@@ -89,7 +90,15 @@ export default function Header() {
 	}, [router]);
 
 	const dataGoogle: any = localStorage?.getItem('dataLogin');
-	const decoded: any = jwtDecode(dataGoogle);
+
+	if (dataGoogle) {
+		const decoded: any = jwtDecode(dataGoogle);
+		setDataDecode(decoded);
+	} else {
+		console.error("No data in localStorage for 'dataLogin'.");
+	}
+
+	// const decoded: any = jwtDecode(dataGoogle);
 
 	const {
 		data: dataProfile,
@@ -259,7 +268,7 @@ export default function Header() {
 							</SheetContent>
 						</Sheet>
 
-						{dataProfile || decoded ? (
+						{dataProfile || dataDecode ? (
 							<DropdownMenu>
 								<DropdownMenuTrigger asChild>
 									<div className='flex items-center gap-2 cursor-pointer'>
@@ -270,14 +279,16 @@ export default function Header() {
 													alt={dataProfile.avatar.name}
 												/>
 											)}
-											{decoded && <AvatarImage src={decoded?.avatar} alt={decoded.name} />}
+											{dataDecode && (
+												<AvatarImage src={dataDecode?.avatar} alt={dataDecode.name} />
+											)}
 											{dataProfile && (
 												<AvatarFallback>{dataProfile.avatar.thumbUrl}</AvatarFallback>
 											)}
-											{decoded && <AvatarFallback>{decoded?.avatar}</AvatarFallback>}
+											{dataDecode && <AvatarFallback>{dataDecode?.avatar}</AvatarFallback>}
 										</Avatar>
 										{dataProfile && <span className='text-xs'>{dataProfile.name}</span>}
-										{decoded && <span className='text-xs'>{decoded?.name}</span>}
+										{dataDecode && <span className='text-xs'>{dataDecode?.name}</span>}
 										{dataProfile?.rank ? (
 											<span
 												className={`px-2 py-1 bg-slate-200 rounded-md ${
