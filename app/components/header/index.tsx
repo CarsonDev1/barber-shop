01ecd46fa8ea8900bf5e97a08a08c60e22e7eda7
CarsonDev1 <buitritinht@gmail.com>
2024-12-18 14:13:44 +0700

    upload codE

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -271,14 +271,14 @@ export default function Header() {
 													alt={dataProfile.avatar.name}
 												/>
 											)}
-											{decoded && <AvatarImage src={decoded.avatar} alt={decoded.name} />}
+											{decoded && <AvatarImage src={decoded?.avatar} alt={decoded.name} />}
 											{dataProfile && (
 												<AvatarFallback>{dataProfile.avatar.thumbUrl}</AvatarFallback>
 											)}
-											{decoded && <AvatarFallback>{decoded.avatar}</AvatarFallback>}
+											{decoded && <AvatarFallback>{decoded?.avatar}</AvatarFallback>}
 										</Avatar>
 										{dataProfile && <span className='text-xs'>{dataProfile.name}</span>}
-										{decoded && <span className='text-xs'>{decoded.name}</span>}
+										{decoded && <span className='text-xs'>{decoded?.name}</span>}
 										{dataProfile?.rank ? (
 											<span
 												className={`px-2 py-1 bg-slate-200 rounded-md ${
